9c532aa7aed478dcd43cc597e4510305b93b91a6
Artem <lunycat@mail.ru>
2025-04-13 23:28:08 +0300

    add filter chain

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,28 +1,44 @@
 package com.example.security.config;
 
+import com.example.security.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-    private final AuthenticationProvider authenticationProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    public void configure(AuthenticationManagerBuilder builder) {
-        builder.authenticationProvider(authenticationProvider);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http
+               .userDetailsService(personDetailsService)
+               .authorizeHttpRequests(manager -> manager
+                       .requestMatchers("/login", "/error").permitAll()
+                       .anyRequest().authenticated())
+               .formLogin(form -> form
+                       .loginPage("/login")
+                       .loginProcessingUrl("/process_login")
+                       .defaultSuccessUrl("/hello", true)
+                       .failureForwardUrl("/login?error"));
+
+       return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
