f6659617efcf643cdb8a21f6fdcc66b64165c351
Artem <lunycat@mail.ru>
2025-04-17 22:09:58 +0300

    add PreAuthorize

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.security.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -27,7 +29,6 @@ public class SecurityConfig {
         http
                 .userDetailsService(personDetailsService)
                 .authorizeHttpRequests(manager -> manager
-                        .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/auth/login", "/auth/registration","/error").permitAll()
                         .anyRequest().hasAnyRole("USER", "ADMIN"))
                 .formLogin(form -> form
--- src/main/java/com/example/security/contollers/HelloController.java
+++ src/main/java/com/example/security/contollers/HelloController.java
@@ -1,10 +1,19 @@
 package com.example.security.contollers;
 
+import com.example.security.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     @GetMapping("/hello")
     public String hello() {
@@ -13,6 +22,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
